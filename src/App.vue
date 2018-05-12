8f03232ce8adb1f1b2838f5e3a307950586e7023
David Ridgley <Arximiro@outlook.com>
2018-05-12 08:34:39 -0400

    add VideoList component, add YouTube API fetch functionality

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,39 @@
 <template>
   <div>
     <SearchBar @termChange="onTermChange"></SearchBar>
+    <VideoList></VideoList>
+    {{ videos.length }}
   </div>
 </template>
 
 
 <script>
+import axios from 'axios';
 import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+
+const API_KEY = 'AIzaSyC5KS5ytOBbDakTfilVoAdD1EDrX7pW9f0';
 
 export default {
   name: 'App',
   components: {
-    SearchBar
+    SearchBar,
+    VideoList
+  },
+  data() {
+    return { videos: [] };
   },
   methods: {
-    onTermChange(searchTerm) {
-      console.log(searchTerm);
+    async onTermChange(searchTerm) {
+      const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
+        params: {
+          key: API_KEY,
+          type: 'video',
+          part: 'snippet',
+          q: searchTerm
+        }
+      });
+      this.videos = response.data.items;
     }
   }
 };
